116f7c83ce5208324eba0f9c612e8de7d2df7762
RachitThakker <84980300+RachitThakker@users.noreply.github.com>
2021-06-04 18:07:19 +0530

    Changed IPs and made more user-friendly

--- Python scripts/scheduler_script.py
+++ Python scripts/scheduler_script.py
@@ -20,11 +20,13 @@ import schedule, time, os
 url1 = "http://192.168.103.35/?button1on"
 url2 = "http://192.168.103.35/?button2off"
 print("Process ID: ", os.getpid(), "\n")
+print("Type following command to stop this script:\ntaskkill /F /PID", os.getpid())
 
 def task():
     print("Scheduled function called.")
     data = urllib.request.urlopen(url1)
     print("Process ID: ", os.getpid(), "\n")
+    print("Type following command in new command prompt to stop this script:\ntaskkill /F /PID", os.getpid())
 
 schedule.every().day.at("04:00").do(task)
 # schedule.every(50).seconds.do(task)
